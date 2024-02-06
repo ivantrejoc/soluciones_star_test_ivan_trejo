81ad8912164af4c23e7664d5687eba33cae0508a
ivantrejoc <IVANJTREJOC@GMAIL.COM>
2024-02-06 07:36:16 -0400

    banner done

--- src/app/components/Figma/Figma.tsx
+++ src/app/components/Figma/Figma.tsx
@@ -3,13 +3,13 @@ import Navbar from "../Navbar/Navbar";
 
 const Figma = () => {
   return (
-    <main className="bg-teal-950  text-gray-200 w-full  h-[3977px] top[-513px] left-[5426px]">
-      <div className="bg-[url('/vector-138.svg')] bg-cover w-[2241px] h-[1001px]">
-        <div className="bg-[url('/stars.png')] bg-cover w-[2241px] h-[1001px]">
+    <section className="bg-teal-950 text-gray-200 w-full  h-fit top[-513px]">
+      <div className="bg-[url('/vector-138.svg')] bg-cover w-full h-fit">
+        <div className="bg-[url('/stars.png')] bg-cover  w-full h-[840px]">
           <Navbar />
-          <div className="flex w-full h-[647px] absolute mt-[160px]">
-            <div className="relative">
-              <h1 className="bg-[url('/ellipse-1551.png')] bg-[right_36rem_bottom_0.1rem] bg-no-repeat w-[961px] h-[252px] text-[95px] uppercase font-extrabold leading-[84px] tracking-tighter text-left ml-[147px] mt-[63px]">
+          <div className="flex w-full h-fit absolute mt-[160px] justify-center">
+            <div className="relative w-[580px] ml[147px] justify-start">
+              <h1 className="bg-[url('/ellipse-1551.png')] bg-[right_40rem_bottom_0.1rem] bg-no-repeat w-[961px] h-[252px] text-[70px] uppercase font-extrabold leading-[84px] tracking-tighter text-left mt-[63px]">
                 Collect
                 <br />
                 Mother Mary <br />
@@ -17,7 +17,7 @@ const Figma = () => {
               </h1>
 
               <p
-                className="ml-[147px] mt-[25px] text-xl font-normal leading-[30px] tracking-[0px] text-left;
+                className="mt-[25px] text-[15px] font-normal leading-[30px] tracking-[0px] text-left;
   font-family: Outfit;"
               >
                 Find the best upcoming and live NFT drops. Moonly provides
@@ -26,23 +26,39 @@ const Figma = () => {
               </p>
               <a
                 href="#"
-                className="bg-gradient-to-t from-yellow-500 via-amber-300 to-amber-300 absolute w-[216px] h-[66px] mt-[31px] ml-[147px] text-neutral-800 inline-flex items-center justify-center rounded-[15px] text-base font-bold leading-5 tracking-[0px] text-center"
+                className="bg-gradient-to-t from-yellow-500 via-amber-300 to-amber-300 absolute w-[216px] h-[66px] mt-[31px] text-neutral-800 inline-flex items-center justify-center rounded-[15px] text-base font-bold leading-5 tracking-[0px] text-center"
               >
                 VER EN OPENSEA
               </a>
+              <div className="w-11 h-[50.54px] ml-[-40px] mt-[85px]">
+                <Image
+                src={"/rays.png"}
+                width={44}
+                height={50.54}
+                alt="rays"
+                />
+              </div>
             </div>
-            <div className="w-[512px] h-[647px]">
+            <div className="w-[512px] h-[647px] ml-2">
               <Image
                 src={"/virgin-2-4.png"}
                 height={647}
                 width={512}
                 alt="virgin"
               />
+              <div className="w-14 h-14 ml-[525px] mt-[-120px]">
+                <Image
+                src={"/stars2.png"}
+                width={56}
+                height={56}
+                alt="stars"
+                />
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </main>
+    </section>
   );
 };
 
--- src/app/components/Navbar/Navbar.tsx
+++ src/app/components/Navbar/Navbar.tsx
@@ -2,27 +2,27 @@ import Image from "next/image";
 
 const Navbar = () => {
   return (
-    <nav>
+    <nav className="w-full flex absolute mr-[145px]">
       <Image
-        className="absolute ml-40 mt-8"
+        className="ml-[135px] mt-[35px]"
         src={"/image-4.png"}
         width={63}
         height={62}
         alt="mother mary logo"
       />
-      <div className="absolute flex w-[453px] h-5 left-[573px] top-[63px] text-gray-200 gap-10">
+      <div className="flex w-[453px] h-5 ml-[160px] mt-[63px] text-gray-200">
         <a
           href="#"
-          className="not-italic font-medium text-base leading-5 text-center text-white"
+          className="w-[43px] not-italic font-medium text-base leading-5 text-center text-white mr-[35px]"
         >
           Home
         </a>
-        <a href="#" className="not-italic font-medium text-base leading-5 text-center text-white">Drops</a>
-        <a href="#" className="not-italic font-medium text-base leading-5 text-center text-white">Marketplace</a>
-        <a href="#" className="not-italic font-medium text-base leading-5 text-center text-white">Activity</a>
-        <a href="#" className="not-italic font-medium text-base leading-5 text-center text-white">Company</a>
+        <a href="#" className="w-[44px] not-italic font-medium text-base leading-5 text-center text-white mr-[39px]">Drops</a>
+        <a href="#" className="w-[90px] not-italic font-medium text-base leading-5 text-center text-white mr-[40px]">Marketplace</a>
+        <a href="#" className="w-[55px] not-italic font-medium text-base leading-5 text-center text-white mr-[37px]">Activity</a>
+        <a href="#" className="w-[70px] not-italic font-medium text-base leading-5 text-center text-white">Company</a>
       </div>
-      <a href="#" className="absolute w-[164px] h-[53px]  top-[47px] border-2 border-amber-300 rounded-2xl text-white not-italic font-normal text-base leading-5 inline-flex items-center justify-center left-[1350px] right-[165px]">Ver en opensea</a>
+      <a href="#" className="w-[164px] h-[53px] mt-[47px] ml-[205px] mr-[90px] border-2 border-amber-300 rounded-2xl text-white not-italic font-normal text-base leading-5 inline-flex items-center justify-center ">Ver en opensea</a>
     </nav>
   );
 };
