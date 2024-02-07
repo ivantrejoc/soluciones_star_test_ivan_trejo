6a02e8c396cdb4b72219682391776b062700ea3b
ivantrejoc <IVANJTREJOC@GMAIL.COM>
2024-02-06 22:48:11 -0400

    prisma connection

--- src/libs/db.ts
+++ src/libs/db.ts
@@ -1,20 +1,26 @@
 //CONEXIÓN CON BASE DE DATOS
 import { PrismaClient } from '@prisma/client'
 
-const prismaClientSingleton = () => {
-  return new PrismaClient()
-}
 
-type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClientSingleton | undefined
-}
-
-const prisma = globalForPrisma.prisma ?? prismaClientSingleton()
+let prisma = new PrismaClient()
 
 
 
-if (process.env.NODE_ENV === 'production') globalForPrisma.prisma = prisma
+// const prismaClientSingleton = () => {
+//   return new PrismaClient()
+// }
+
+// type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>
+
+// const globalForPrisma = globalThis as unknown as {
+//   prisma: PrismaClientSingleton | undefined
+// }
+
+// const prisma = globalForPrisma.prisma ?? prismaClientSingleton()
+
+
+
+// if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
 
 export const db = prisma;
